4cd50c92400ecf174b78756286547d91f8c5874b
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2026-03-18 14:57:16 +0400

    try 2

--- project_portfolio/next.config.ts
+++ project_portfolio/next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/WebsitePortfolio',  // MUST match your repo name
+  assetPrefix: '/WebsitePortfolio/',  // Add this for assets
   images: {
     unoptimized: true,
   },
-  /* config options here */
 };
 
 export default nextConfig;
